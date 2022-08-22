164ba037a81c1940d9ba1b4bdfb4fd3ad1ddc29d
RadinTiholov <radin.tiholov@mail.bg>
2022-08-22 18:59:55 +0300

    Fix header and add logo

--- Client/movie-hut/src/components/Header/Header.js
+++ Client/movie-hut/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import styles from './Header.module.css';
+import logo from '../../images/logo.jpg'
 
 export const Header = () => {
     return (
         <nav className={`navbar navbar-expand-lg ${styles.navbarBackground}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Navbar
+                <a className="navbar-brand mx-3" to="/">
+                    <img src={logo} alt="" width={45} height={45} />
                 </a>
                 <button
                     className="navbar-toggler"
@@ -19,50 +20,21 @@ export const Header = () => {
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className={`navbar-nav me-auto mb-2 mb-lg-0`}>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
+                            <a className="nav-link" aria-current="page" href="#">
                                 Home
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">
-                                Link
+                                Movies
                             </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
+                            <a className="nav-link" href="#">
+                                Actors
+                            </a>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
