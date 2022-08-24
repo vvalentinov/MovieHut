7ce283b6e69f3c548ec438056f3e33d09f7882a1
Valentin <valentinvalentinov027@gmail.com>
2022-08-24 13:56:03 +0300

    Initial Create Project

---
+++ Server/MovieHut/MovieHut/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieHut.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
