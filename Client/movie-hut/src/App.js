1805169b96570c905186338851d9347ee730cf5f
RadinTiholov <radin.tiholov@mail.bg>
2022-08-24 16:17:00 +0300

    Add routing

--- Client/movie-hut/src/App.js
+++ Client/movie-hut/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
+import {
+    Routes,
+    Route
+} from "react-router-dom";
+
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Footer/>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                {/* <Route path='/login' element={} />
+                <Route path='/register' element={} /> */}
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
