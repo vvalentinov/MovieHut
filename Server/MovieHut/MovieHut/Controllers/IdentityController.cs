cb24dcb7c467e58541572eec08bfb55970002bb2
Valentin <valentinvalentinov027@gmail.com>
2022-08-24 14:23:49 +0300

    Add Register Logic

--- Server/MovieHut/MovieHut/Controllers/IdentityController.cs
+++ Server/MovieHut/MovieHut/Controllers/IdentityController.cs
@@ -1,6 +1,36 @@
 ﻿namespace MovieHut.Controllers
 {
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using MovieHut.Data.Models;
+    using MovieHut.Models.Identity;
+
     public class IdentityController : ApiController
     {
+        private readonly UserManager<User> userManager;
+
+        public IdentityController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [Route(nameof(Register))]
+        public async Task<ActionResult> Register(RegisterUserRequestModel model)
+        {
+            var user = new User()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            var result = await this.userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+        }
     }
 }
---
+++ Server/MovieHut/MovieHut/Models/Identity/RegisterUserRequestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieHut.Models.Identity
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterUserRequestModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
