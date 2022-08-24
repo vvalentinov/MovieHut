4fb7bbf603e88205b6ebd2b44aa5f2031384cf04
Valentin <valentinvalentinov027@gmail.com>
2022-08-24 14:59:21 +0300

    Add Kestrel Endpoints in appsettings.json

--- Server/MovieHut/MovieHut/Controllers/HomeController.cs
+++ Server/MovieHut/MovieHut/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿namespace MovieHut.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : ApiController
     {
+        [Authorize]
         public ActionResult Get()
         {
             return Ok("It's working!");
