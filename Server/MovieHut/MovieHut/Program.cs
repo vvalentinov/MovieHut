bda85f2c181051a51bbe6d214f4de4d591900546
Valentin <valentinvalentinov027@gmail.com>
2022-08-24 14:13:59 +0300

    Install JWT Bearer Package and Add JWT Authentication in the builder Services Container

--- Server/MovieHut/MovieHut/Program.cs
+++ Server/MovieHut/MovieHut/Program.cs
@@ -1,9 +1,12 @@
 namespace MovieHut
 {
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
     using MovieHut.Data;
     using MovieHut.Data.Models;
+    using System.Text;
 
     public class Program
     {
@@ -18,6 +21,31 @@ namespace MovieHut
 
             builder.Services.AddIdentity<User, IdentityRole>();
             builder.Services.AddControllers();
+
+            // Application Settings
+            var applicationSettingsConfiguration = builder.Configuration.GetSection("ApplicationSettings");
+            builder.Services.Configure<AppSettings>(applicationSettingsConfiguration);
+
+            // Registers authentication with JWT
+            var appSettings = applicationSettingsConfiguration.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
 
             var app = builder.Build();
 
