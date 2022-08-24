09ce25b5a578e7c1e52476021624aca477c7b983
Valentin <valentinvalentinov027@gmail.com>
2022-08-24 14:26:25 +0300

    Add Login Logic

--- Server/MovieHut/MovieHut/Controllers/IdentityController.cs
+++ Server/MovieHut/MovieHut/Controllers/IdentityController.cs
@@ -2,19 +2,26 @@
 {
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using Microsoft.IdentityModel.Tokens;
     using MovieHut.Data.Models;
     using MovieHut.Models.Identity;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
 
     public class IdentityController : ApiController
     {
         private readonly UserManager<User> userManager;
+        private readonly AppSettings appSettings;
 
-        public IdentityController(UserManager<User> userManager)
+        public IdentityController(UserManager<User> userManager, IOptions<AppSettings> appSettings)
         {
             this.userManager = userManager;
+            this.appSettings = appSettings.Value;
         }
 
-        [Route(nameof(Register))]
+        [Route("register")]
         public async Task<ActionResult> Register(RegisterUserRequestModel model)
         {
             var user = new User()
@@ -31,6 +38,43 @@
             }
 
             return BadRequest(result.Errors);
+        }
+
+        [Route("login")]
+        public async Task<ActionResult<object>> Login(LoginUserRequestModel model)
+        {
+            var user = await this.userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var passwordValid = await this.userManager.CheckPasswordAsync(user, model.Password);
+
+            if (passwordValid == false)
+            {
+                return Unauthorized();
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var encryptedToken = tokenHandler.WriteToken(token);
+
+            return new
+            {
+                Token = encryptedToken,
+            };
         }
     }
 }
---
+++ Server/MovieHut/MovieHut/Models/Identity/LoginUserRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieHut.Models.Identity
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginUserRequestModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
