71a9b5a9077d3c89468496315a92582d18c974d8
Valentin <valentinvalentinov027@gmail.com>
2022-08-25 14:18:24 +0300

    Create IIdentityService with IdentityService class

---
+++ Server/MovieHut/MovieHut/Features/Identity/IIdentityService.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieHut.Features.Identity
+{
+    public interface IIdentityService
+    {
+        string GenerateJwtToken(string userId, string userName, string secret);
+    }
+}
---
+++ Server/MovieHut/MovieHut/Features/Identity/IdentityService.cs
@@ -0,0 +1,31 @@
+﻿namespace MovieHut.Features.Identity
+{
+    using Microsoft.IdentityModel.Tokens;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+
+    public class IdentityService : IIdentityService
+    {
+        public string GenerateJwtToken(string userId, string userName, string secret)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId),
+                    new Claim(ClaimTypes.Name, userName)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var encryptedToken = tokenHandler.WriteToken(token);
+
+            return encryptedToken;
+        }
+    }
+}
