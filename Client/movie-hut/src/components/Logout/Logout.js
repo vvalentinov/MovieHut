c7334dd5add0258751f4786f95280148fe2817f1
RadinTiholov <radin.tiholov@mail.bg>
2022-08-25 16:40:26 +0300

    Add logout functionality

---
+++ Client/movie-hut/src/components/Logout/Logout.js
@@ -0,0 +1,15 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+
+export const Logout = () => {
+    const navigate = useNavigate();
+    const { userLogout } = useContext(AuthContext);
+    useEffect(() => {
+        userLogout();
+        localStorage.clear();
+        navigate('/');
+    }, [])
+
+    return null;
+}
