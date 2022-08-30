c702ec35f02b0adc896ed45b4775dd1161ad5792
RadinTiholov <radin.tiholov@mail.bg>
2022-08-30 17:24:43 +0300

    Fix sizing

--- Client/movie-hut/src/components/Card/Card.js
+++ Client/movie-hut/src/components/Card/Card.js
@@ -3,8 +3,8 @@ import './Card.css'
 
 export const Card = (props) => {
     return (
-        <div className="col-auto grow "><div className="card" style={{ width: 300 }}>
-            <img className="card-img-top" src={props.posterUrl} alt="Card img" />
+        <div className="col-auto grow "><div className="card" style={{ width: 300}}>
+            <img className="card-img-top" src={props.posterUrl} alt="Card img" style={{ height: 400}}/>
             <div className="card-body">
                 <h4 className="card-title">{props.title}</h4>
                 <Link to={`/movies/details/${props.id}`} className="custom-btn">
