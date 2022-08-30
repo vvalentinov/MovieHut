6183a60314378322c6f1331b34146e1ba49f7530
RadinTiholov <radin.tiholov@mail.bg>
2022-08-30 17:06:51 +0300

    Fix positioning in grid

--- Client/movie-hut/src/components/MyMovies/MyMovies.js
+++ Client/movie-hut/src/components/MyMovies/MyMovies.js
@@ -5,8 +5,8 @@ export const MyMovies = () => {
         <>
             <p className="text-center display-5">Your Movies</p>
             <div className="container my-5">
-                <div className="row gy-5">
-                    <div className="col-sm grow"><div className="card" style={{ width: 300}}>
+                <div className="row justify-content-center gy-5">
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
                         <div className="card-body">
                             <h4 className="card-title">John Doe</h4>
@@ -16,7 +16,7 @@ export const MyMovies = () => {
                         </div>
                     </div>
                     </div>
-                    <div className="col-sm grow"><div className="card" style={{ width: 300}}>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
                         <div className="card-body">
                             <h4 className="card-title">John Doe</h4>
@@ -26,7 +26,7 @@ export const MyMovies = () => {
                         </div>
                     </div>
                     </div>
-                    <div className="col-sm grow"><div className="card" style={{ width: 300}}>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
                         <div className="card-body">
                             <h4 className="card-title">John Doe</h4>
@@ -36,7 +36,37 @@ export const MyMovies = () => {
                         </div>
                     </div>
                     </div>
-                    <div className="col-sm grow"><div className="card" style={{ width: 300}}>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
+                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
+                        <div className="card-body">
+                            <h4 className="card-title">John Doe</h4>
+                            <a href="#" className="custom-btn">
+                                See Profile
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
+                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
+                        <div className="card-body">
+                            <h4 className="card-title">John Doe</h4>
+                            <a href="#" className="custom-btn">
+                                See Profile
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
+                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
+                        <div className="card-body">
+                            <h4 className="card-title">John Doe</h4>
+                            <a href="#" className="custom-btn">
+                                See Profile
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
                         <div className="card-body">
                             <h4 className="card-title">John Doe</h4>
