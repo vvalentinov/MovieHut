a9dfd2d75aa0442d786f256ccb99d4589928aa9a
RadinTiholov <radin.tiholov@mail.bg>
2022-08-30 17:13:27 +0300

    Create Card component

---
+++ Client/movie-hut/src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import './Card.css'
+
+export const Card = (props) => {
+    return (
+        <div className="col-auto grow "><div className="card" style={{ width: 300 }}>
+            <img className="card-img-top" src={props.imageUrl} alt="Card img" />
+            <div className="card-body">
+                <h4 className="card-title">{props.title}</h4>
+                <Link to={`/movies/details/${props.id}`} className="custom-btn">
+                    Details
+                </Link>
+            </div>
+        </div>
+        </div>
+    )
+}
--- Client/movie-hut/src/components/MyMovies/MyMovies.js
+++ Client/movie-hut/src/components/MyMovies/MyMovies.js
@@ -1,81 +1,13 @@
+import { Card } from '../Card/Card'
 import './MyMovies.css'
 
 export const MyMovies = () => {
     return (
         <>
-            <p className="text-center display-5">Your Movies</p>
+            <p className="text-center display-5">My Movies</p>
             <div className="container my-5">
                 <div className="row justify-content-center gy-5">
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-auto grow "><div className="card" style={{ width: 300}}>
-                        <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU" alt="Card image" />
-                        <div className="card-body">
-                            <h4 className="card-title">John Doe</h4>
-                            <a href="#" className="custom-btn">
-                                See Profile
-                            </a>
-                        </div>
-                    </div>
-                    </div>
+                    <Card/>
                 </div>
             </div>
         </>
