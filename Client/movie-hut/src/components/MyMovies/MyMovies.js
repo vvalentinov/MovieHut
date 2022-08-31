fae6f0f6a25e51de8fe758afc630a5cef541bad9
RadinTiholov <radin.tiholov@mail.bg>
2022-08-31 15:35:31 +0300

    Add exceptional message

--- Client/movie-hut/src/components/MyMovies/MyMovies.js
+++ Client/movie-hut/src/components/MyMovies/MyMovies.js
@@ -12,10 +12,15 @@ export const MyMovies = () => {
     }, [])
     return (
         <>
-            <p className="text-center display-5">My Movies</p>
+            <p className="text-center display-4">My Movies</p>
             <div className="container my-5">
                 <div className="row justify-content-center gy-5">
-                    {movies?.map(x => <Card key = {x.id} {...x}/>)}
+                    {movies?.length > 0
+                        ? movies?.map(x => <Card key={x.id} {...x} />)
+                        : <>
+                            <p className="text-center display-6">You haven't created any movies yet.</p>
+                            <div style={{ height: '100vh' }}>
+                            </div> </>}
                 </div>
             </div>
         </>
