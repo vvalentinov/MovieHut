dfbe2bdc23f3072096b9ba8c13ff79a85d7e579f
Valentin <valentinvalentinov027@gmail.com>
2022-09-01 13:42:46 +0300

    Add AutoMapper to Project and Create Map between Movie and MovieDetailsServiceModel

---
+++ Server/MovieHut/MovieHut/Infrastructure/Profiles/MovieProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieHut.Infrastructure.Profiles
+{
+    using AutoMapper;
+    using MovieHut.Data.Models;
+    using MovieHut.Features.Movies.Models;
+
+    public class MovieProfile : Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<Movie, MovieDetailsServiceModel>();
+        }
+    }
+}
