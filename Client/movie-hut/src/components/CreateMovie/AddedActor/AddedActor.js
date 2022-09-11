60a2b4ee6d4d3d427cc38669356c9ab9afdd432d
RadinTiholov <radin.tiholov@mail.bg>
2022-09-11 21:37:05 +0300

    Change color

--- Client/movie-hut/src/components/CreateMovie/AddedActor/AddedActor.js
+++ Client/movie-hut/src/components/CreateMovie/AddedActor/AddedActor.js
@@ -1,5 +1,5 @@
 export const AddedActor = (props) => {
     return (
-        <span className="badge rounded-pill bg-primary mx-1">{props.name}<i onClick={() => props.removeActor(props.id)} className="fa-solid fa-xmark mx-1"></i></span>
+        <span className="badge rounded-pill mx-1" style={{backgroundColor: '#32CD32'}}>{props.name}<i onClick={() => props.removeActor(props.id)} className="fa-solid fa-xmark mx-1"></i></span>
     )
 }
