074d5f9fea85a73088a533ebec348051766c8a5a
Valentin <valentinvalentinov027@gmail.com>
2022-09-24 11:18:13 +0300

    Add CreateMovieShowRequestBaseModel

---
+++ Server/MovieHut/MovieHut/Features/Base/RequestModels/CreateMovieShowRequestBaseModel.cs
@@ -0,0 +1,42 @@
+﻿namespace MovieHut.Features.Base.RequestModels
+{
+    using MovieHut.Infrastructure.Attributes;
+    using System.ComponentModel.DataAnnotations;
+    using static Infrastructure.ErrorMessages.ModelsValidationErrors.MovieErrors;
+    using static Data.Validation.Movie;
+
+    public abstract class CreateMovieShowRequestBaseModel
+    {
+        [Required(ErrorMessage = RequiredTitleError)]
+        [MinLength(MinTitleLength, ErrorMessage = MinTitleLengthError)]
+        [MaxLength(MaxTitleLength, ErrorMessage = MaxTitleLengthError)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = RequiredPlotError)]
+        [MinLength(MinPlotLength, ErrorMessage = MinPlotLengthError)]
+        [MaxLength(MaxPlotLength, ErrorMessage = MaxPlotLengthError)]
+        public string Plot { get; set; }
+
+        [Required(ErrorMessage = RequiredTrailerUrlError)]
+        [Url(ErrorMessage = ValidTrailerUrlError)]
+        public string TrailerUrl { get; set; }
+
+        [Required(ErrorMessage = RequiredReleasedError)]
+        public DateTime Released { get; set; }
+
+        [Required(ErrorMessage = RequiredPosterUrlError)]
+        public string PosterUrl { get; set; }
+
+        [Required(ErrorMessage = RequiredUserIdError)]
+        public string UserId { get; set; }
+
+        [NotNullOrEmptyIntegerCollection(ErrorMessage = GenresError)]
+        public IEnumerable<int> GenresIds { get; set; }
+
+        [NotNullOrEmptyIntegerCollection(ErrorMessage = ActorsError)]
+        public IEnumerable<int> ActorsIds { get; set; }
+
+        [NotNullOrEmptyIntegerCollection(ErrorMessage = DirectorsError)]
+        public IEnumerable<int> DirectorsIds { get; set; }
+    }
+}
--- Server/MovieHut/MovieHut/Features/Movies/Models/CreateMovieRequestModel.cs
+++ Server/MovieHut/MovieHut/Features/Movies/Models/CreateMovieRequestModel.cs
@@ -1,45 +1,13 @@
 ﻿namespace MovieHut.Features.Movies.Models
 {
-    using MovieHut.Infrastructure.Attributes;
+    using MovieHut.Features.Base.RequestModels;
     using System.ComponentModel.DataAnnotations;
     using static Data.Validation.Movie;
     using static Infrastructure.ErrorMessages.ModelsValidationErrors.MovieErrors;
 
-    public class CreateMovieRequestModel
+    public class CreateMovieRequestModel : CreateMovieShowRequestBaseModel
     {
-        [Required(ErrorMessage = RequiredTitleError)]
-        [MinLength(MinTitleLength, ErrorMessage = MinTitleLengthError)]
-        [MaxLength(MaxTitleLength, ErrorMessage = MaxTitleLengthError)]
-        public string Title { get; set; }
-
-        [Required(ErrorMessage = RequiredPlotError)]
-        [MinLength(MinPlotLength, ErrorMessage = MinPlotLengthError)]
-        [MaxLength(MaxPlotLength, ErrorMessage = MaxPlotLengthError)]
-        public string Plot { get; set; }
-
         [Range(MinDuration, MaxDuration, ErrorMessage = DurationRangeError)]
         public int Duration { get; set; }
-
-        [Required(ErrorMessage = RequiredTrailerUrlError)]
-        [Url(ErrorMessage = ValidTrailerUrlError)]
-        public string TrailerUrl { get; set; }
-
-        [Required(ErrorMessage = RequiredReleasedError)]
-        public DateTime Released { get; set; }
-
-        [Required(ErrorMessage = RequiredPosterUrlError)]
-        public string PosterUrl { get; set; }
-
-        [Required(ErrorMessage = RequiredUserIdError)]
-        public string UserId { get; set; }
-
-        [NotNullOrEmptyIntegerCollection(ErrorMessage = GenresError)]
-        public IEnumerable<int> GenresIds { get; set; }
-
-        [NotNullOrEmptyIntegerCollection(ErrorMessage = ActorsError)]
-        public IEnumerable<int> ActorsIds { get; set; }
-
-        [NotNullOrEmptyIntegerCollection(ErrorMessage = DirectorsError)]
-        public IEnumerable<int> DirectorsIds { get; set; }
     }
 }
