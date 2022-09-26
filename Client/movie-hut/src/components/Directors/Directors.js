a7f310343c66702f75f280c61cef8d172478beb9
RadinTiholov <radin.tiholov@mail.bg>
2022-09-26 19:53:06 +0300

    Add directors filtering

--- Client/movie-hut/src/components/Directors/Directors.js
+++ Client/movie-hut/src/components/Directors/Directors.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { DirectorContext } from '../../contexts/DirectorContext'
 import { Missing } from '../Missing/Missing'
 import { DirectorCard } from './DirectorCard/DirectorCard'
@@ -6,6 +7,7 @@ import styles from './Directors.module.css'
 
 export const Directors = () => {
     const {directors} = useContext(DirectorContext);
+    const [searchParams, setSearchParams] = useSearchParams();
     return (
         <div className="container">
             <div className="row gy-3 my-2 justify-content-center">
@@ -14,7 +16,22 @@ export const Directors = () => {
                         <div className='card-body'>
                             <p className='display-6'>Directors</p>
                             <div className='container'>
-                                {directors.length > 0 ? directors.map(x => <DirectorCard key = {x.id} {...x}/>) : <Missing message = "No Directors yet."/>}
+                            {directors.length > 0 
+                                    ? directors?.map(x => {
+                                        if(searchParams.get('search')){
+                                            let searchQuery = (searchParams.get('search')).toLowerCase();
+                                            if (x?.name?.toLowerCase().includes(searchQuery)){
+                                                return <DirectorCard key={x.id} {...x} />
+                                            }else{
+                                                return null;
+                                            }
+                                        }
+                                        else {
+                                            return <DirectorCard key={x.id} {...x} />
+                                        }
+
+                                    })
+                                    : <Missing message = "No Direcotrs yet."/>}
                             </div>
                         </div>
                     </div>
